9e4a4baeae442f43b0f9a7e822194d82a945eb77
qazal <qazal.software@gmail.com>
2023-09-02 17:08:45 +0300

    squashed some dev work in
    commit 052bcaf22486fc6eb5958673f404a10fb2a24236
    Author: qazal <qazal.software@gmail.com>
    Date:   Sat Sep 2 17:07:15 2023 +0300
    
        add more rules
    
    commit a2bec6fa01f21ed4eb0d52f8e0986d384bb819a8
    Merge: 9dcea4c 185ac46
    Author: qazal <qazal.software@gmail.com>
    Date:   Sat Sep 2 16:57:27 2023 +0300
    
        Merge branch 'main' into dev
    
    commit 9dcea4c3e47d94d9afa28f9914d52bb2d54c6958
    Author: qazal <qazal.software@gmail.com>
    Date:   Sat Sep 2 16:51:55 2023 +0300
    
        remove everything from yt and add more features
    
    commit fffd3f5039635931cb2b196d44c5c99306198832
    Author: qazal <qazal.software@gmail.com>
    Date:   Sat Sep 2 12:52:16 2023 +0300
    
        this might be better to go in another plugin

--- src/js/chatgpt.js
+++ src/js/chatgpt.js
@@ -2,7 +2,8 @@ const removeClasses = [
   "text-gray-400 flex self-end lg:self-center justify-center mt-2 gap-2 md:gap-3 lg:gap-1 lg:absolute lg:top-0 lg:translate-x-full lg:right-0 lg:mt-0 lg:pl-2 visible",
   "relative pb-3 pt-2 text-center text-xs text-gray-600 dark:text-gray-300 md:px-[60px] md:pb-6 md:pt-3",
   "h-full flex ml-1 md:w-full md:m-auto md:mb-4 gap-0 md:gap-2 justify-center",
-    "flex flex-1 flex-grow items-center gap-1 p-1 text-gray-600 dark:text-gray-200 sm:justify-center sm:p-0"
+  "flex flex-1 flex-grow items-center gap-1 p-1 text-gray-600 dark:text-gray-200 sm:justify-center sm:p-0",
+  "relative pb-3 pt-2 text-center text-xs text-gray-600 dark:text-gray-300 md:px-[60px] md:pb-6 md:pt-3",
 ];
 
 for (const classes of removeClasses) {
@@ -26,3 +27,99 @@ const el3 = document.querySelector(
   "div[class='flex flex-col w-full py-[10px] flex-grow md:py-4 md:pl-4 relative border border-black/10 bg-white dark:border-gray-900/50 dark:text-white dark:bg-gray-700 rounded-xl shadow-xs dark:shadow-xs']",
 );
 el3.style.marginBottom = "5px";
+
+const grayDark = {
+  grayRadix1: "hsl(0, 0%, 9.5%)",
+  grayRadix2: "hsl(0, 0%, 10.5%)",
+  grayRadix3: "hsl(0, 0%, 15.8%)",
+  grayRadix4: "hsl(0, 0%, 18.9%)",
+  grayRadix5: "hsl(0, 0%, 21.7%)",
+  grayRadix6: "hsl(0, 0%, 24.7%)",
+  grayRadix7: "hsl(0, 0%, 29.1%)",
+  grayRadix8: "hsl(0, 0%, 37.5%)",
+  grayRadix9: "hsl(0, 0%, 43.0%)",
+  grayRadix10: "hsl(0, 0%, 50.7%)",
+  grayRadix11: "hsl(0, 0%, 69.5%)",
+  grayRadix12: "hsl(0, 0%, 93.5%)",
+};
+const mauveDark = {
+  mauve1: "hsl(300, 5.0%, 9.5%)",
+  mauve2: "hsl(300, 7.1%, 11.0%)",
+  mauve3: "hsl(290, 6.2%, 16.2%)",
+  mauve4: "hsl(285, 5.8%, 19.5%)",
+  mauve5: "hsl(280, 5.6%, 22.4%)",
+  mauve6: "hsl(276, 5.4%, 25.7%)",
+  mauve7: "hsl(269, 5.3%, 30.3%)",
+  mauve8: "hsl(258, 5.0%, 39.2%)",
+  mauve9: "hsl(250, 5.0%, 45.0%)",
+  mauve10: "hsl(252, 4.3%, 52.3%)",
+  mauve11: "hsl(250, 6.0%, 70.5%)",
+  mauve12: "hsl(240, 7.0%, 93.8%)",
+};
+
+const slateDark = {
+  slate1: "hsl(240, 5.0%, 9.8%)",
+  slate2: "hsl(240, 6.9%, 11.4%)",
+  slate3: "hsl(227, 6.7%, 16.4%)",
+  slate4: "hsl(222, 6.6%, 19.4%)",
+  slate5: "hsl(218, 6.5%, 22.1%)",
+  slate6: "hsl(214, 6.4%, 25.1%)",
+  slate7: "hsl(209, 6.2%, 29.4%)",
+  slate8: "hsl(202, 5.8%, 37.5%)",
+  slate9: "hsl(220, 6.0%, 44.0%)",
+  slate10: "hsl(218, 5.3%, 51.5%)",
+  slate11: "hsl(220, 7.0%, 70.0%)",
+  slate12: "hsl(220, 7.0%, 93.5%)",
+};
+
+const sageDark = {
+  sage1: "hsl(155, 7.0%, 9.2%)",
+  sage2: "hsl(150, 7.7%, 10.2%)",
+  sage3: "hsl(151, 5.5%, 15.2%)",
+  sage4: "hsl(152, 4.7%, 18.3%)",
+  sage5: "hsl(153, 4.2%, 21.1%)",
+  sage6: "hsl(153, 3.7%, 24.2%)",
+  sage7: "hsl(154, 3.3%, 28.7%)",
+  sage8: "hsl(156, 2.6%, 37.1%)",
+  sage9: "hsl(155, 6.0%, 41.5%)",
+  sage10: "hsl(157, 4.6%, 49.2%)",
+  sage11: "hsl(155, 5.0%, 68.3%)",
+  sage12: "hsl(155, 7.0%, 93.0%)",
+};
+
+const oliveDark = {
+  olive1: "hsl(110, 5.0%, 9.2%)",
+  olive2: "hsl(120, 5.7%, 10.4%)",
+  olive3: "hsl(120, 4.3%, 15.4%)",
+  olive4: "hsl(120, 3.9%, 18.5%)",
+  olive5: "hsl(120, 3.6%, 21.2%)",
+  olive6: "hsl(120, 3.3%, 24.3%)",
+  olive7: "hsl(120, 3.0%, 28.7%)",
+  olive8: "hsl(120, 2.6%, 37.1%)",
+  olive9: "hsl(110, 6.0%, 41.5%)",
+  olive10: "hsl(106, 4.6%, 49.3%)",
+  olive11: "hsl(110, 5.0%, 68.8%)",
+  olive12: "hsl(110, 7.0%, 93.0%)",
+};
+
+const sandDark = {
+  sand1: "hsl(60, 6.0%, 9.0%)",
+  sand2: "hsl(60, 1.9%, 10.4%)",
+  sand3: "hsl(56, 2.4%, 15.4%)",
+  sand4: "hsl(53, 2.7%, 18.4%)",
+  sand5: "hsl(51, 2.9%, 21.2%)",
+  sand6: "hsl(49, 3.1%, 24.3%)",
+  sand7: "hsl(47, 3.3%, 28.7%)",
+  sand8: "hsl(43, 3.7%, 37.1%)",
+  sand9: "hsl(50, 4.0%, 41.8%)",
+  sand10: "hsl(48, 3.5%, 49.5%)",
+  sand11: "hsl(50, 5.0%, 68.3%)",
+  sand12: "hsl(56, 7.0%, 93.0%)",
+};
+
+const grays = [grayDark, mauveDark, slateDark, sageDark, oliveDark, sandDark];
+for (const gray of grays) {
+  Object.keys(gray).forEach((key) => {
+    document.documentElement.style.setProperty(`--${key}`, gray[key]);
+  });
+}
