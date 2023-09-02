58487ba810e72243ff2c1283da3a3d9b4f87e291
qazal <qazal.software@gmail.com>
2023-09-02 10:14:33 +0300

    make it _modular_

--- src/background.js
+++ src/background.js
@@ -1,28 +1,43 @@
-const extensions = 'https://developer.chrome.com/docs/extensions'
-const webstore = 'https://developer.chrome.com/docs/webstore'
+const cssDir = "src/css";
+// Mapping apps to css files that disable specific parts of the app
+const fileAppMap = {
+  "https://mail.google.com": "gmail.css",
+  "https://docs.google.com": "docs.css",
+  "https://discord.com/channels": "discord.css",
+  "https://www.notion.so": "notion.css",
+  "https://app.slack.com/": "slack.css",
+  "https://open.spotify.com/": "spotify.css",
+  "https://developer.chrome.com/docs/extensions": "chrome.css",
+  "https://www.youtube.com": "youtube.css",
+};
 
 chrome.action.onClicked.addListener(async (tab) => {
-  if (tab.url.startsWith(extensions) || tab.url.startsWith(webstore)) {
-    const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
-    const nextState = prevState === 'ON' ? 'OFF' : 'ON'
-
-    // Set the action badge to the next state
-    await chrome.action.setBadgeText({
-      tabId: tab.id,
-      text: nextState,
-    });
-    if (nextState === "ON") {
-      // Insert the CSS file when the user turns the extension on
-      await chrome.scripting.insertCSS({
-        files: ["src/focus-mode.css"],
-        target: { tabId: tab.id },
-      });
-    } else if (nextState === "OFF") {
-      // Remove the CSS file when the user turns the extension off
-      await chrome.scripting.removeCSS({
-        files: ["src/focus-mode.css"],
-        target: { tabId: tab.id },
-      });
-    }
+  const supported_app = Object.keys(fileAppMap).find((app) =>
+    tab.url.startsWith(app),
+  );
+  if (supported_app == null) {
+    console.log(`App not supported: ${tab.url}`);
+    return;
   }
+  const cssFile = `${cssDir}/${fileAppMap[supported_app]}`;
+
+  const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
+  const active = prevState === "ON" ? false : true;
+  // cutting edge state mngnt
+  await chrome.action.setBadgeText({
+    tabId: tab.id,
+    text: active ? "ON" : "OFF",
+  });
+
+  if (!active) {
+    await chrome.scripting.removeCSS({
+      files: [cssFile],
+      target: { tabId: tab.id },
+    });
+    return;
+  }
+  await chrome.scripting.insertCSS({
+    files: [cssFile],
+    target: { tabId: tab.id },
+  });
 });
