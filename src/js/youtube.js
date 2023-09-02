20663aaad0d8779f728c8751b92bba1e0cd1a115
qazal <qazal.software@gmail.com>
2023-09-02 16:51:55 +0300

    remove everything from yt and add more features

--- src/js/youtube.js
+++ src/js/youtube.js
@@ -1,31 +1,29 @@
-// remove the stupid shorts
-const elements = Array.from(document.querySelectorAll("#dismissible"));
-const filteredElements = elements.filter((element) => {
-  return Array.from(element.children).some((child) =>
-    child.textContent.includes("Shorts"),
-  );
-});
-filteredElements.forEach((element) => {
-  element.remove();
-});
+if (window.location.href === "https://www.youtube.com/") {
+  document.querySelector("#primary").remove();
 
-// select groups of channels i actually get value from
-const whitelistChannels = [
-  "History",
-  "Watch later",
-  "Downloads",
-  "magnetic minds",
-  "All was well",
-];
-const sidebarElements = Array.from(
-  document.querySelectorAll("ytd-guide-entry-renderer"),
-);
-const innerTexts = sidebarElements.map((element) => element.innerText);
-const shouldRemove = sidebarElements.filter((element) => {
-  return !whitelistChannels.some((channel) => {
-    return element.innerText.includes(channel);
+  // select groups of channels i actually get value from
+  const whitelistChannels = [
+    "History",
+    "Watch later",
+    "Downloads",
+    "magnetic minds",
+    "All was well",
+  ];
+  const sidebarElements = Array.from(
+    document.querySelectorAll("ytd-guide-entry-renderer"),
+  );
+  const shouldRemove = sidebarElements.filter((element) => {
+    return !whitelistChannels.some((channel) => {
+      return element.innerText.includes(channel);
+    });
   });
-});
-shouldRemove.forEach((element) => {
-  element.remove();
-});
+  shouldRemove.forEach((element) => {
+    element.remove();
+  });
+}
+
+if (window.location.href.startsWith("https://www.youtube.com/watch?v=")) {
+  // remove anything in the side
+  document.querySelector("#secondary").remove();
+  document.querySelector("#below").remove();
+}
